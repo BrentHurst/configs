7628c2e8414148c3c11a095f78748a93e52dccec
Brent Hurst <GBrentHurst@gmail.com>
2019-08-20 12:28:49 -0400

    Added a line to load msvc stuff

--- windows/.aliases.cmd
+++ windows/.aliases.cmd
@@ -25,4 +25,7 @@ doskey mail="C:\Users\hurstgb\Documents\Shortcuts\Mail-Shortcut.lnk"
 
 doskey u=ubuntu1804
 
+REM For loading MSVC stuff
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+
 cls
